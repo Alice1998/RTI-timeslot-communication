d6b237d8224df142f39e61dbf4d58bc9d38ce831
Alice1998 <alicerdfz@163.com>
2020-05-13 21:26:03 +0800

    serial changed in windows. add data here. timer0 tested. tbd timeslot change

--- data/analysis.py
+++ data/analysis.py
@@ -7,41 +7,59 @@ import numpy as np
 import matplotlib
 
 def get_timestamp(input):
+    m_s=input.split('-')
+    s_micro=m_s[1].split('.')
+    return int(m_s[0],10)*60+int(s_micro[0],10)+int(s_micro[1],10)*0.001
+
+def central_get_timestamp(input):
     h_m_s=input.split('-')
     s_micro=h_m_s[2].split('.')
     return int(h_m_s[0],10)*3600+int(h_m_s[1],10)*60+int(s_micro[0],10)+int(s_micro[1],10)*0.000001
 
-time_stamp=0
-last_send_rsp=0
-round_counter=[{},{},{}]
 data=[]
-with open("14-30-18.641000_sensor2.csv",'r') as f:
-    reader=csv.reader(f)
-    for row in reader:
-        if len(row)==2:
-            if row[1]=="[R]: 20 ":
-                time_stamp=get_timestamp(row[0])
-                #print(time_stamp)
-            elif len(row[1])==len("[R]: 1 "):
-                index=int(row[1][5],16)
-                #if index==3 and time<0.1:
-                #    data.append(time)
-                if index>0 and index<4:
-                    time=get_timestamp(row[0])
-                    if round(time-last_send_rsp,3)<0.006 and index!=1:
-                        data.append(round(time-last_send_rsp,3))
-                    last_send_rsp=time
-                '''
-                if time in round_counter[index-1]:
-                    round_counter[index-1][time]+=1
-                else:
-                    round_counter[index-1][time]=1
-                '''
-            elif row[1]=="[R]: 50 ":
-                time=round(get_timestamp(row[0])-time_stamp,3)
-                #if time<0.08:
-                #    data.append(time)
-#print(round_counter)
+def analysis_sensor():
+    global data
+    time_stamp=0
+    last_send_rsp=0
+    with open("19-51-08.138000_sensor1.csv",'r') as f:
+    #with open("19-51-16.402000_sensor2.csv",'r') as f:
+        reader=csv.reader(f)
+        for row in reader:
+            if len(row)==2:
+                if row[1]=="20 \n":
+                    time_stamp=get_timestamp(row[0])
+                    #print(time_stamp)
+                elif len(row[1])==len("1 \n"):
+                    index=int(row[1][0],10)
+                    #if index>0 and index<3:
+                    #    time=get_timestamp(row[0])
+                    #    if index!=1 and time-last_send_rsp<0.02:
+                    #       data.append(time-last_send_rsp)
+                    #    last_send_rsp=time
+                elif row[1]=="50 \n":
+                    time=round(get_timestamp(row[0])-time_stamp,3)
+                    if time<0.01:
+                        data.append(time)
+                elif row[1]=="10 \n":
+                    time=round(get_timestamp(row[0])-last_send_rsp,3)
+                    #if time<0.025 and last_send_rsp!=0:
+                    #    data.append(time)
+    #print(round_counter)
+
+def analysis_central():
+    time_stamp=0
+    with open("09-46-14.737000_central.csv",'r') as f:
+        reader=csv.reader(f)
+        for row in reader:
+            if len(row)==2 and len(row[1])>0:
+                if row[1][0]==" ":
+                    time=central_get_timestamp(row[0])
+                    #print(time,time_stamp,time-time_stamp)
+                    if time-time_stamp<0.1:
+                        data.append(time-time_stamp)
+                    time_stamp=time
+analysis_sensor()
+#analysis_central()
 
 plt.hist(data, bins=5000, normed=0, facecolor="blue", edgecolor="black", alpha=0.7)
 # 显示横轴标签
